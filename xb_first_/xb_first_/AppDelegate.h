0ad71f9599b9efdc46ccc0220c4eb2d5dde68818
apple <apple@appledeMacBook-Pro.local>
2016-05-06 18:07:21 +0800

    初始化项目

---
+++ xb_first_/xb_first_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xb_first_
+//
+//  Created by apple on 16/5/6.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xb_first_/xb_first_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xb_first_
+//
+//  Created by apple on 16/5/6.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
